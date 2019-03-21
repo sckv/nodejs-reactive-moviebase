a1bce992b7b7a1724bf5feaf69a9eff0073c713e
Konstantin Knyazev <konstantinknyazev@outlook.com>
2019-03-21 15:59:25 +0100

    (add) updating for all packages + babel module resolver

---
+++ babel.config.front.js
@@ -0,0 +1,32 @@
+const path = require('path');
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        targets: {node: process.versions.node},
+      },
+    ],
+  ];
+  const plugins = [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: path.resolve(path.join(process.cwd(), 'client', 'build')),
+        root: '.',
+        extensions: ['.js'],
+        alias: {
+          '@src': '.',
+          '@config': 'config',
+        },
+      },
+    ],
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
+};
