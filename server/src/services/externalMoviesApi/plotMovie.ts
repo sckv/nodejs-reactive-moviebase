0809fef560a0ffc0436fe6ef4a4257d51180ace8
Konstantin Knyazev <konstantinknyazev@outlook.com>
2019-04-02 19:11:48 +0200

    (change) conditional in plotMovie refactor

--- server/src/services/externalMoviesApi/plotMovie.ts
+++ server/src/services/externalMoviesApi/plotMovie.ts
@@ -3,15 +3,11 @@ import {OMDBMovieResponse} from '@src/services/externalMoviesApi/externalMovies'
 const OMDB_URL = `http://www.omdbapi.com/?apikey=${process.env.OMBD_API_KEY}&i=`;
 
 export const plotMovie = async (ttId: string): Promise<OMDBMovieResponse | null> => {
-  const response = await rq({
+  const response = (await rq({
     url: OMDB_URL + ttId,
     method: 'GET',
     json: true,
-  });
+  })) as OMDBMovieResponse;
 
-  if (response.Response) {
-    return response as OMDBMovieResponse;
-  } else {
-    return null;
-  }
+  return response.Response ? response : null;
 };
