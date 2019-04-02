6e357a8626c13e372588802238761e6d3431d836
Konstantin Knyazev <konstantinknyazev@outlook.com>
2019-04-02 19:50:18 +0200

    (add) pm2 config #13

---
+++ pm2.production.js
@@ -0,0 +1,85 @@
+module.exports = {
+  apps: [
+    {
+      name: 'movies',
+      script: './movies.js',
+      instances: 2,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'cache',
+      script: './cache.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'users',
+      script: './users.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '200M',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'auth',
+      script: './auth.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '200M',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'emails',
+      script: './emails.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '300M',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  // deploy: {
+  //   production: {
+  //     user: 'node',
+  //     host: '212.83.163.1',
+  //     ref: 'origin/master',
+  //     repo: 'git@github.com:repo.git',
+  //     path: '/var/www/production',
+  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+  //   },
+  // },
+};
