a8fe536ceba346006037e995ab4f842617943a77
Konstantin Knyazev <k-stormcode@outlook.com>
2019-05-12 02:03:00 +0200

    resolves #13 #12 #8 #7 #6 #5 #4 #3 (#75)
    * (add) infrastructure service layer, routing, envoy, handlers, repositories, resolves #13 #12 #8 #7 #6 #5 #4 #3
    
    * (add) infrastructure service layer, routing, envoy, handlers, repositories, resolves #13 #12 #8 #7 #6 #5 #4 #3

--- server/src/routes/userControlling/index.ts
+++ server/src/routes/userControlling/index.ts
@@ -4,13 +4,13 @@ import * as UsersControlling from '@src/handlers/users-controlling.handlers';
 import {AuthRouteGuard} from '@src/utils/authorization-guard';
 
 const index: RouteController = app => {
-  app.route('/users/activate/:token').post(asyncWrapper(UsersControlling.activate));
-  app.route('/users/register').post(asyncWrapper(UsersControlling.register));
-  app.route('/users/:userId').post(asyncWrapper(UsersControlling.getUserData));
-  app.route('/users/:userId').patch(asyncWrapper(AuthRouteGuard(UsersControlling.modifyUser)));
-  app.route('/users/follow').post(asyncWrapper(UsersControlling.followUser));
-  app.route('/users/unfollow').post(asyncWrapper(UsersControlling.unfollowUser));
-  app.route('/users').get(asyncWrapper(UsersControlling.searchUsers));
+  app.route('/activate/:token').post(asyncWrapper(UsersControlling.activate));
+  app.route('/register').post(asyncWrapper(UsersControlling.register));
+  app.route('/:userId').post(asyncWrapper(UsersControlling.getUserData));
+  app.route('/:userId').patch(asyncWrapper(AuthRouteGuard(UsersControlling.modifyUser)));
+  app.route('/follow').post(asyncWrapper(UsersControlling.followUser));
+  app.route('/unfollow').post(asyncWrapper(UsersControlling.unfollowUser));
+  app.route('/').get(asyncWrapper(UsersControlling.searchUsers));
 };
 
 export = index;
